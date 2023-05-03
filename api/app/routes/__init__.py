2945f4d7368f7f8d325c6d757d76c77e90cc632d
Gakawarstone <pinnekerv@gmail.com>
2023-05-03 07:43:06 +0200

    feat: add get subs list route

--- api/app/routes/__init__.py
+++ api/app/routes/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from .add import add_feed
 from .feed import get_rss_feed
+from .list import get_subscriptions_list
 
 
 def setup(app: FastAPI):
     app.add_api_route('/feed', get_rss_feed)
     app.add_api_route('/add', add_feed)
+    app.add_api_route('/list', get_subscriptions_list)
