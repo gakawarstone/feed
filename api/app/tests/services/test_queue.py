522910a61f072a2d9fe512b9e141d2bf6a7be064
Gakawarstone <pinnekerv@gmail.com>
2023-07-06 22:05:31 +0200

    fix: hide import (depends on env)

--- api/app/tests/services/test_queue.py
+++ api/app/tests/services/test_queue.py
@@ -1,7 +1,5 @@
 import pytest
 
-from app.services.queue import QueueService
-
 
 async def _async_add(a: int, b: int) -> int:
     return a + b
@@ -9,6 +7,8 @@ async def _async_add(a: int, b: int) -> int:
 
 @pytest.mark.skip(reason='meaningless with mocks without works not in CI')
 async def test_get_result():
+    from app.services.queue import QueueService
+
     expected_result = 3
 
     result = await QueueService.put_and_wait_for_result(_async_add, [1, 2])
