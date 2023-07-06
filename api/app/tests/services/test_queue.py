52bf2423e266b095eea2da12f0ac12d16e9a2e17
Gakawarstone <pinnekerv@gmail.com>
2023-07-06 21:52:32 +0200

    fix: test queue

--- api/app/tests/services/test_queue.py
+++ api/app/tests/services/test_queue.py
@@ -1,3 +1,8 @@
+import pytest
+from redis import Redis
+from redis.exceptions import ConnectionError
+
+from app.settings import REDIS_HOST
 from app.services.queue import QueueService
 
 
@@ -6,6 +11,11 @@ async def _async_add(a: int, b: int) -> int:
 
 
 async def test_get_result():
+    try:
+        Redis(host=REDIS_HOST).ping()
+    except ConnectionError:
+        pytest.skip('Redis is unreachable')
+
     expected_result = 3
 
     result = await QueueService.put_and_wait_for_result(_async_add, [1, 2])
